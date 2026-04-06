d5af464c9d9c5eb0186c7e143908034d065ecb18
angelruiz230 <angel.ruiz4650@alumnos.udg.mx>
2026-04-05 21:31:17 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// Función para el botón interactivo
+function mostrarMensaje() {
+    const msj = document.getElementById('mensaje-oculto');
+    msj.classList.toggle('show');
+}
+
+// Lógica para contar los días (Ajusta la fecha a la suya)
+const fechaInicio = new Date('2026-03-21'); // Año-Mes-Día
+
+function actualizarContador() {
+    const ahora = new Date();
+    const diferencia = ahora - fechaInicio;
+    
+    // Convertir milisegundos a días
+    const dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+    
+    document.getElementById('contador').innerText = `Llevamos ${dias} días creando recuerdos juntos.`;
+}
+
+// Ejecutar al cargar
+actualizarContador();
